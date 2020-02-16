6e62a4698c99ec1763ab8a41d619e486f52944c3
Andreas_Sujono <andreassujono@gmail.com>
2020-02-17 02:51:54 +0800

    Updates

---
+++ precache-manifest.dd62f0b69606ac754eca106db630ceb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40479859ec2eabf74fa5ef6f9b2844e8",
+    "url": "/reduxtagram/index.html"
+  },
+  {
+    "revision": "380a7b4ebbc686f5eda3",
+    "url": "/reduxtagram/static/css/main.0c41a408.chunk.css"
+  },
+  {
+    "revision": "4c4a398f6ac30d33abd3",
+    "url": "/reduxtagram/static/js/2.f648d776.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reduxtagram/static/js/2.f648d776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380a7b4ebbc686f5eda3",
+    "url": "/reduxtagram/static/js/main.1d534a20.chunk.js"
+  },
+  {
+    "revision": "189f83aad46163b1d3d5",
+    "url": "/reduxtagram/static/js/runtime-main.ed70c30f.js"
+  }
+]);
